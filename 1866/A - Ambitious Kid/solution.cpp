64991cfc1fc418911635f8ceb8d03483de927917
Nikhil kumar <nikhilkumar.cout@gmail.com>
2026-02-03 16:50:17 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(NULL);        int n;        cin>>n;        vector<int>a(n);        for(int i=0;i<n;i++){            cin>>a[i];        }        int ans=abs(a[0]);        for(int i=1;i<n;i++){            ans=min(ans,abs(a[i]));        }        cout<<ans<<'\n';     return 0;}
