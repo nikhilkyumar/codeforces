04b0f8f430b02721a4ed59c49ce391a0b8b9b3e4
Nikhil kumar <nikhilkumar.cout@gmail.com>
2026-02-12 17:22:53 +0530

    Add Coins [A] from Codeforces

---
+++ 1814/A - Coins/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(NULL);     int t;    cin>>t;    while(t--){      long long n,k;      cin>>n>>k;      int rem=n%k;      if(n%2==0||(n-k)%2==0){        cout<<"YES \n";          }else{      cout<<"NO \n";    }     }    return 0;}
