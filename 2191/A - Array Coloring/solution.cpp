8b82d19fce115ff84eb24d5176b47a5e1030d411
Nikhil kumar <nikhilkumar.cout@gmail.com>
2026-09-06 13:14:11 +0530

    Add Array Coloring [A] from Codeforces

--- 2191/A - Array Coloring/solution.cpp
+++ 2191/A - Array Coloring/solution.cpp
@@ -1,1 +1,30 @@
-#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(NULL);     int t;    cin>>t;    while(t--){        long long n;        cin>>n;        vector<long long> a(n);        for(int i=0;i<n;i++){          cin>>a[i];        }        bool fact= true;        for(int i=0;i+1<n;i++){            if(a[i]%2==a[i+1]%2){                fact= false;            }        }        if(fact==true) cout<<"YES \n";        else cout<<"NO \n";    }     return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long> a(n);
+        for(int i=0;i<n;i++){
+          cin>>a[i];
+        }
+        bool fact= true;
+        for(int i=0;i+1<n;i++){
+            if(a[i]%2==a[i+1]%2){
+                fact= false;
+            }
+        }
+        if(fact==true) cout<<"YES 
+";
+        else cout<<"NO 
+";
+    }
+ 
+    return 0;
+}
