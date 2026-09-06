b85e5ef03a22ef27025dad7d635decbb106db621
Nikhil kumar <nikhilkumar.cout@gmail.com>
2026-09-06 13:15:11 +0530

    Add Buttons [A] from Codeforces

--- 1858/A - Buttons/solution.cpp
+++ 1858/A - Buttons/solution.cpp
@@ -1,1 +1,38 @@
-#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(NULL);     int t;    cin>>t;    while(t--){        int arr[3];        for(int i=0;i<3;i++){            cin>>arr[i];        }        int a=arr[0];        int b=arr[1];        int c=arr[2];        if(a>b){             cout<<"First\n";}         else if(b>a){         cout<<"Second \n";}         else if(a==b){            if(c%2==0) cout<<"Second  \n";            else{                cout<<"First \n";            }        }     }     return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int arr[3];
+        for(int i=0;i<3;i++){
+            cin>>arr[i];
+        }
+        int a=arr[0];
+        int b=arr[1];
+        int c=arr[2];
+        if(a>b){
+             cout<<"First
+";}
+ 
+        else if(b>a){
+         cout<<"Second 
+";}
+ 
+        else if(a==b){
+            if(c%2==0) cout<<"Second  
+";
+            else{
+                cout<<"First 
+";
+            }
+        }
+ 
+    }
+ 
+    return 0;
+}
